85c681f2f2cea48d3e6d6a9f87b46ed2ac780d4d
cihadacar <>
2026-02-18 10:33:33 +0300

    implement order service with message queue
    also remove application.properties from tracking

---
+++ src/main/java/com/customer/order/model/Orders.java
@@ -0,0 +1,30 @@
+package com.customer.order.model;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+public class Orders {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @JsonProperty("customer_number")
+    private String customerNumber;
+    @JsonProperty("product_id")
+    private String productId;
+    @JsonProperty("quantity")
+    private int quantity;
+    @JsonProperty("order_date")
+    private LocalDateTime orderDate;
+
+    public void setOrderDate(LocalDateTime orderDate) {
+        this.orderDate = orderDate;
+    }
+
+    public LocalDateTime getOrderDate() {
+        return this.orderDate;
+    }
+
+}
